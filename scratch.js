1263c57c64a356b36c68598b4974af4cf24d73e2
spencer kelly <spencerkelly86@gmail.com>
2022-06-07 12:16:29 -0400

    es examples

--- scratch.js
+++ scratch.js
@@ -12,7 +12,33 @@ txt = `Nosotras comimos los zapatos calientes`
 txt = `Ellas comen el zapato, nosotras comimos`
 txt = `tiramos nuestros zapatos al río`
 
-let doc = nlp('tengo cuarenta dolares')
+
+txt = `Qué irónico`
+txt = `Algo irónico`
+txt = `no solo fue impactante.` //impactar
+txt = `disgustado por circunstancias` //desagradar
+txt = `Aprovecha el poder ` //aprovechar
+txt = `La incorporación estatutaria siguió` //estatutario
+txt = `Está ahí para la exploración espacial` //exploración
+txt = `Cualquiera puede decir cosas encantadoras y tratar de` //encantar
+txt = `Las 10 principales conversiones patrimoniales en Toronto` //conversión
+txt = `la compañía se dispersó` //dispersarse
+txt = `una apelación oportuna.` //oportuno
+txt = `Estamos viendo algunas señales alentadoras.` //alentador
+txt = `Profundo en esa oscuridad mirando` //mirar
+txt = `Determina si has estado abusando del alcohol` //abusar
+txt = `republicanos que simplifiquen el sistema` //simplificar
+txt = `Voy a usar el cojín.`//'cojín'
+txt = `Los cojines son la esencia`//'cojín'
+txt = `Alguien me dijo que soy condescendiente.`//condescender
+txt = `Uno asesinado; 6 heridos en disputa familiar`//'disputar'
+txt = `Las líneas de preocupación aparecieron con la edad.`//preocupación'
+txt = ` AGO Massive Party un espectáculo extraño y maravilloso`//espectáculo'
+txt = ` Las papas fritas estaban empapadas`//empapado'
+txt = ` AGO Massive Party un espectáculo extraño y maravilloso`//espectáculo'
+txt = ` las reclamaciones restantes fallan porque no están maduras.`//madura'
+
+let doc = nlp('la persona se enoja fácilmente o es verbalmente abusiva.')
 doc.numbers().minus(50)
 doc.text()
 // tengo moins diez dolares
