f6a7f36d173dfd04cfc62ba0a145f369cf0f2a72
spencer kelly <spencerkelly86@gmail.com>
2022-07-29 13:50:04 -0400

    script to generate adjective model

---
+++ scripts/generate-model.js
@@ -0,0 +1,21 @@
+import { learn, compress } from 'suffix-thumb'
+import data from '/Users/spencer/mountain/es-compromise/data/models/adjectives.js'
+
+let out = {}
+let pairs = data.map(a => [a[0], a[1]])
+let model = learn(pairs)
+model = compress(model)
+out.toFem = model
+
+pairs = data.map(a => [a[0], a[2]])
+model = learn(pairs)
+model = compress(model)
+out.toPlural = model
+
+pairs = data.map(a => [a[0], a[3]])
+model = learn(pairs)
+model = compress(model)
+out.toFemPlural = model
+
+
+console.log(out)
