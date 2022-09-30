710754b3d3ea518bb5dac0ee4cbc36083b85db67
spencer kelly <spencerkelly86@gmail.com>
2022-09-30 14:45:51 -0400

    update deps

--- scratch.js
+++ scratch.js
@@ -15,28 +15,6 @@ import data from '/Users/spencer/mountain/es-compromise/data/models/adjectives.j
 
 /*
 */
-// console.log(data.length)
-// let missing = data.filter(a => {
-//   let doc = nlp(a[0])
-//   return !doc.has('#Adjective') && !doc.has('#Value')
-// })
-// missing = missing.map(a => a[0])
-// console.log(missing.length)
-// const suffixSort = function (arr) {
-//   const reverse = (str = '') => str.split('').reverse().join('')
-//   return arr.sort((a, b) => {
-//     a = reverse(a)
-//     b = reverse(b)
-//     if (a > b) {
-//       return 1
-//     } else if (a < b) {
-//       return -1
-//     }
-//     return 0
-//   })
-// }
-
-// console.log(JSON.stringify(suffixSort(missing), null, 2))
 
 let txt = `en direcciones opuestas`
 let doc = nlp(txt)
