8ce319d357ebe0ae11f7631403d857b9d05b9d73
spencer kelly <spencermountain@gmail.com>
2023-08-24 16:40:01 -0400

    remove german

--- add-verbs.js
+++ add-verbs.js
@@ -1,4 +1,4 @@
-import prettyJSON from 'pretty-json-stringify'
+// import prettyJSON from 'pretty-json-stringify'
 import already from './data/models/verb/present-tense.js'
 
 import fs from 'fs'
@@ -11,21 +11,24 @@ let arr = fs
   .filter((str) => str)
   .map((str) => JSON.parse(str))
 
-let out = {}
-let count = arr.forEach((obj) => {
-  if (
-    obj.word &&
-    obj['Indicativo Presente'] &&
-    obj['Indicativo Presente'].length === 6 &&
-    !already[obj.word]
-  ) {
-    out[obj.word] = obj['Indicativo Presente']
-  } else {
-    // console.log(obj['Indicativo Presente'])
-  }
-})
-console.log(
-  prettyJSON(out, {
-    shouldExpand: (_, level) => (level >= 1 ? false : true),
-  })
-)
+let out = arr.map((o) => o.word)
+console.log(JSON.stringify(out, null, 2))
+
+// let out = {}
+// let count = arr.forEach((obj) => {
+//   if (
+//     obj.word &&
+//     obj['Indicativo Presente'] &&
+//     obj['Indicativo Presente'].length === 6 &&
+//     !already[obj.word]
+//   ) {
+//     out[obj.word] = obj['Indicativo Presente']
+//   } else {
+//     // console.log(obj['Indicativo Presente'])
+//   }
+// })
+// console.log(
+//   prettyJSON(out, {
+//     shouldExpand: (_, level) => (level >= 1 ? false : true),
+//   })
+// )
